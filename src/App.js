6ce916890855dceaeb0e9284023d998a3cd6c6da
Nelba Barreto <nbarreto@excelsis.com.py>
2024-02-03 21:57:33 -0300

    Agregar foto y linkedin

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import foto from "./img/foto.jpg";
+import "./App.css";
 
-function App() {
+const App = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={foto} className="App-logo" alt="foto nelba barreto" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello! I am Nelba Barreto.
         </p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
+          className=""
+          href="https://www.linkedin.com/in/nelba-barreto/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Linkedin
         </a>
       </header>
     </div>
