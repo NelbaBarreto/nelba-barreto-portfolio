65dcee3f55e18da57834b4d54e9fdd211c42a71b
Nelba Barreto <nbarreto@excelsis.com.py>
2024-02-03 22:27:44 -0300

    Instalar tailwind, corregir fuente y foto

--- src/App.js
+++ src/App.js
@@ -5,12 +5,12 @@ const App = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={foto} className="App-logo" alt="foto nelba barreto" />
-        <p>
-          Hello! I am Nelba Barreto.
-        </p>
+        <img src={foto} className="h-80 rounded-full" alt="foto nelba barreto" />
+        <h1 className="text-3xl font-bold">
+          Hello! I'm Nelba Barreto
+        </h1>
         <a
-          className=""
+          className="underline text-blue-600 hover:text-blue-800"
           href="https://www.linkedin.com/in/nelba-barreto/"
           target="_blank"
           rel="noopener noreferrer"
