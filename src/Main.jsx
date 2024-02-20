291f9b058693a9810d70a588e5af9a411e2ec8df
Nelba Barreto <nbarreto@excelsis.com.py>
2024-02-19 21:03:54 -0300

    Add useContext to manage selected section

---
+++ src/Main.jsx
@@ -0,0 +1,26 @@
+import "./App.css";
+import React, { useState } from "react";
+import { SectionContext } from "./context.js";
+import Descripcion from "./components/Header.jsx";
+import Navbar from "./components/Navbar.jsx";
+import Projects from "./components/Projects.jsx";
+import AboutMe from "./components/AboutMe.jsx";
+import Contact from "./components/Contact.jsx";
+
+const App = () => {
+  const [section, setSection] = useState(0);
+
+  return (
+    <SectionContext.Provider value={{ section, setSection }}>
+      <div className="bg-background-50 min-h-screen">
+        <Navbar />
+        <Descripcion />
+        <AboutMe />
+        <Projects />
+        <Contact />
+      </div>
+    </SectionContext.Provider>
+  );
+}
+
+export default App;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,21 +1,39 @@
-import React from "react";
-
-const navbarItemClassName = "navbar-item text-background-50 font-semibold text-lg";
+import React, { useContext } from "react";
+import { SectionContext } from "../context";
 
 const Navbar = () => {
+  const {section, setSection} = useContext(SectionContext);
+  const navbarItemClassName = "navbar-item text-background-50 font-semibold text-lg";
+
   return (
     <nav className="navbar bg-retro-150 border-b-retro-100 border-b-4">
       <div className="navbar-start">
-        <a className={navbarItemClassName} href="#home">
+        <a 
+          className={navbarItemClassName} 
+          href="#home"
+          onClick={() => setSection(0)}
+        >
           Home
         </a>
-        <a className={navbarItemClassName} href="#about-me">
+        <a 
+          className={navbarItemClassName} 
+          href="#about-me"
+          onClick={() => setSection(1)}
+        >
           About me
         </a>
-        <a className={navbarItemClassName} href="#projects">
+        <a 
+          className={navbarItemClassName} 
+          href="#projects"
+          onClick={() => setSection(2)}
+        >
           Projects
         </a>
-        <a className={navbarItemClassName} href="#contact">
+        <a 
+          className={navbarItemClassName} 
+          href="#contact"
+          onClick={() => setSection(3)}
+        >
           Contact
         </a>
       </div>
