3e983f684497217934edd6e80123e3f8002708cc
Nelba Barreto <nbarreto@excelsis.com.py>
2024-02-20 16:26:33 -0300

    Add confetti effect on name click

---
+++ src/components/Confetti.jsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+import Realistic from "react-canvas-confetti/dist/presets/realistic";
+
+const Description = () => {
+  const [conductor, setConductor] = useState();
+
+  const onOnce = () => {
+    conductor?.shoot();
+  };
+
+  const onInit = ({ conductor }) => {
+    setConductor(conductor);
+  };
+
+  return (
+    <div className="text-white">
+      <h1 className="title sm:is-3 is-1 text-white font-bold">
+        Hello! I'm&nbsp;
+        <button 
+          className="text-retro-150"
+          onClick={onOnce}
+        >
+        Nelba
+        </button> 
+        &#127752;
+      </h1>
+      <Realistic onInit={onInit} />
+      <section>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      </section>
+    </div>
+  )
+}
+
+export default Description;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,28 +1,16 @@
 import React from "react";
 import foto from "../img/foto.jpg"
-
-const Description = () => {
-  return (
-    <div className="text-white">
-      <h1 className="title is-1 text-white font-bold">
-        Hello! I'm <span className="text-retro-150">Nelba</span> &#127752;
-      </h1>
-      <section>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-      </section>
-    </div>
-  )
-}
+import Confetti from "./Confetti";
 
 const Header = () => {
   return (
     <header className="container pt-5" id="home">
       <div className="columns">
-        <div className="column is-one-quarter">
+        <div className="column is-one-quarter mb-10">
           <img src={foto} className="h-60 rounded-full" alt="foto nelba barreto" />
         </div>
         <div className="column">
-          <Description />
+          <Confetti />
         </div>
       </div>
     </header>
