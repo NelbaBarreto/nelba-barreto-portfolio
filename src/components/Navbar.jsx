e2143186070003c3923e44e6319f3b7e488355ec
Nelba Barreto <nbarreto@excelsis.com.py>
2024-02-19 21:20:05 -0300

    Change navbar-item color when selected

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,36 +1,37 @@
 import React, { useContext } from "react";
 import { SectionContext } from "../context";
+import classNames from "classnames";
 
 const Navbar = () => {
-  const {section, setSection} = useContext(SectionContext);
+  const { section, setSection } = useContext(SectionContext);
   const navbarItemClassName = "navbar-item text-background-50 font-semibold text-lg";
 
   return (
     <nav className="navbar bg-retro-150 border-b-retro-100 border-b-4">
       <div className="navbar-start">
-        <a 
-          className={navbarItemClassName} 
+        <a
+          className={classNames(navbarItemClassName, { "bg-retro-200 text-white": section === 0 })}
           href="#home"
           onClick={() => setSection(0)}
         >
           Home
         </a>
-        <a 
-          className={navbarItemClassName} 
+        <a
+          className={classNames(navbarItemClassName, { "bg-retro-200 text-white": section === 1 })}
           href="#about-me"
           onClick={() => setSection(1)}
         >
           About me
         </a>
-        <a 
-          className={navbarItemClassName} 
+        <a
+          className={classNames(navbarItemClassName, { "bg-retro-200 text-white": section === 2 })}
           href="#projects"
           onClick={() => setSection(2)}
         >
           Projects
         </a>
-        <a 
-          className={navbarItemClassName} 
+        <a
+          className={classNames(navbarItemClassName, { "bg-retro-200 text-white": section === 3 })}
           href="#contact"
           onClick={() => setSection(3)}
         >
