a251c6b1bd48b16e5940f28bf1ea96e177a100cf
Nelba Barreto <nbarreto@excelsis.com.py>
2024-02-22 19:15:20 -0300

    Style hamburger's dropdown menu

---
+++ src/components/utils.js
@@ -0,0 +1,6 @@
+export const options = [
+  { id: 0, label: "Home", href: "#home" },
+  { id: 1, label: "About Me", href: "#about-me" },
+  { id: 2, label: "Projects", href: "#projects" },
+  { id: 3, label: "Contact", href: "#contact" },
+]
