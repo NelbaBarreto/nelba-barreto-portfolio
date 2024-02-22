089e52057432784a3fc5cf9c677fea349e8a4f3e
Nelba Barreto <nbarreto@excelsis.com.py>
2024-02-22 15:35:07 -0300

    Correct navbar default styles without bulma

--- src/components/Navbar2.jsx
+++ src/components/Navbar2.jsx
@@ -11,87 +11,81 @@ const Navbar = () => {
   const navbarItemClassName = "navbar-item text-background-50 font-semibold text-lg";
 
   return (
-    <nav className="bg-retro-150 border-b-retro-100 border-b-4 h-11">
-      <div className="container mx-auto flex justify-between items-center">
-        <div className="hidden sm:flex space-x-4">
-          <div className="navbar-menu">
-            <div className="navbar-start">
-              <a
-                className={classNames(navbarItemClassName, { "bg-retro-200 text-white": section === 0 })}
-                href="#home"
-                onClick={() => setSection(0)}
-              >
-                Home
-              </a>
-              <a
-                className={classNames(navbarItemClassName, { "bg-retro-200 text-white": section === 1 })}
-                href="#about-me"
-                onClick={() => setSection(1)}
-              >
-                About Me
-              </a>
-              <a
-                className={classNames(navbarItemClassName, { "bg-retro-200 text-white": section === 2 })}
-                href="#projects"
-                onClick={() => setSection(2)}
-              >
-                Projects
-              </a>
-              <a
-                className={classNames(navbarItemClassName, { "bg-retro-200 text-white": section === 3 })}
-                href="#contact"
-                onClick={() => setSection(3)}
-              >
-                Contact
-              </a>
+    <nav className="navbar bg-retro-150 border-b-retro-100 border-b-4">
+      <div className="navbar-menu hidden">
+        <div className="navbar-start">
+          <a
+            className={classNames(navbarItemClassName, { "bg-retro-200 text-white": section === 0 })}
+            href="#home"
+            onClick={() => setSection(0)}
+          >
+            Home
+          </a>
+          <a
+            className={classNames(navbarItemClassName, { "bg-retro-200 text-white": section === 1 })}
+            href="#about-me"
+            onClick={() => setSection(1)}
+          >
+            About Me
+          </a>
+          <a
+            className={classNames(navbarItemClassName, { "bg-retro-200 text-white": section === 2 })}
+            href="#projects"
+            onClick={() => setSection(2)}
+          >
+            Projects
+          </a>
+          <a
+            className={classNames(navbarItemClassName, { "bg-retro-200 text-white": section === 3 })}
+            href="#contact"
+            onClick={() => setSection(3)}
+          >
+            Contact
+          </a>
+        </div>
+        <div className="navbar-end">
+          <div className="navbar-item">
+            <div className="buttons my-auto">
+              <button>
+                <a
+                  className="text-3xl text-background-50 mx-2"
+                  href="https://www.linkedin.com/in/nelba-barreto/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <FontAwesomeIcon icon={faLinkedin} />
+                </a>
+              </button>
+              <button>
+                <a
+                  className="text-3xl text-background-50 mx-2"
+                  href="https://github.com/NelbaBarreto/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <FontAwesomeIcon icon={faGithub} />
+                </a>
+              </button>
+              <button>
+                <a
+                  className="text-3xl text-background-50 mx-2"
+                  href="mailto:barretonelba@gmail.com"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <FontAwesomeIcon icon={faEnvelope} />
+                </a>
+              </button>
             </div>
-            {/* <div className="navbar-end">
-              <div className="navbar-item">
-                <div className="buttons my-auto">
-                  <button>
-                    <a
-                      className="text-3xl text-background-50 mx-2"
-                      href="https://www.linkedin.com/in/nelba-barreto/"
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      <FontAwesomeIcon icon={faLinkedin} />
-                    </a>
-                  </button>
-                  <button>
-                    <a
-                      className="text-3xl text-background-50 mx-2"
-                      href="https://github.com/NelbaBarreto/"
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      <FontAwesomeIcon icon={faGithub} />
-                    </a>
-                  </button>
-                  <button>
-                    <a
-                      className="text-3xl text-background-50 mx-2"
-                      href="mailto:barretonelba@gmail.com"
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      <FontAwesomeIcon icon={faEnvelope} />
-                    </a>
-                  </button>
-                </div>
-              </div>
-            </div> */}
           </div>
         </div>
-
-        <div className="sm:hidden">
-          <button className="sm:hidden h-10 my-auto">
-            <FontAwesomeIcon className="text-3xl text-background-50 mx-2" icon={faBurger} />
-          </button>
-        </div>
       </div>
-    </nav>
-
+      <div className="sm:hidden">
+        <button className="sm:hidden my-auto">
+          <FontAwesomeIcon className="text-3xl text-background-50 mx-2" icon={faBurger} />
+        </button>
+      </div>
+    </nav >
   )
 }
 
