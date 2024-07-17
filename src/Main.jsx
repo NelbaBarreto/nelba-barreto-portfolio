74b4c5a0fdbba3c8f4528fe256152c3ff54edd35
Nelba Barreto <barretonelba@gmail.com>
2024-07-17 10:05:57 -0400

    Agregar react-router

--- src/Main.jsx
+++ src/Main.jsx
@@ -6,23 +6,44 @@ import Navbar from "./components/Navbar.jsx";
 import Projects from "./components/Projects.jsx";
 import Contact from "./components/Contact.jsx";
 import Certifications from "./components/Certifications.jsx";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const Main = () => {
+  return (
+    <>
+      <Navbar />
+      <div className="container">
+        <div className="lg:m-0 mx-4">
+          <Descripcion />
+          <Projects />
+          <Certifications />
+          <Contact />
+        </div>
+      </div>
+    </>
+  )
+}
 
 const App = () => {
   const [section, setSection] = useState(0);
 
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Main />,
+    },
+  ]);
+
   return (
     <SectionContext.Provider value={{ section, setSection }}>
       <div className="bg-background-50 min-h-screen">
-        <Navbar />
-        <div className="container">
-          <div className="lg:m-0 mx-4">
-            <Descripcion />
-            <Projects />
-            <Certifications />
-            <Contact />
-          </div>
-        </div>
-      </div>
+        <React.StrictMode>
+          <RouterProvider router={router} />
+        </React.StrictMode>
+      </div >
     </SectionContext.Provider>
   );
 }
