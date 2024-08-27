bc10d555aa0c2d1c84b3bd0d2dac10b68bb2863e
Nelba Barreto <barretonelba@gmail.com>
2024-08-27 11:23:58 -0400

    Mostrar top tracks a mediano plazo

--- src/temp/Spotify.jsx
+++ src/temp/Spotify.jsx
@@ -6,10 +6,13 @@ const Spotify = () => {
 
   return (
     <section className="bg-gray-800 rounded-lg mt-5 p-8 border border-gray-700 shadow-lg" id="projects">
-      <p className="text-white mb-6">
-        I'm still working on this project, but meanwhile, you can look at my top 50 Spotify tracks.
+      <p className="text-white">
+      I'm still working on this project, but in the meantime, check out my top 50 Spotify tracks, which I retrieved using Azure Data Factory (ADF).
       </p>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+      <code className="text-gray-400">
+        https://api.spotify.com/v1/me/top/artists?time_range=medium_term&limit=50&offset=0
+      </code>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 mt-5">
         {items.map((item, idx) => (
           <div key={idx} className="bg-gray-900 p-4 rounded-lg shadow-md">
             <img
@@ -18,6 +21,7 @@ const Spotify = () => {
               className="w-full h-32 object-cover rounded-md mb-4"
             />
             <h3 className="text-white text-lg font-semibold">{item.name}</h3>
+            <p className="text-white">{item.artists[0].name}</p>
             <p className="text-gray-400">{item.album.name}</p>
           </div>
         ))}
