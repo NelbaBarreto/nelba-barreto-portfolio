b09b5c5e3596ddb1113e049f421213e888bbd833
Nelba Barreto <barretonelba@gmail.com>
2024-08-27 13:13:59 -0400

    Hacer url de la api responsive

--- src/temp/Spotify.jsx
+++ src/temp/Spotify.jsx
@@ -7,9 +7,9 @@ const Spotify = () => {
   return (
     <section className="bg-gray-800 rounded-lg mt-5 p-8 border border-gray-700 shadow-lg" id="projects">
       <p className="text-white">
-      I'm still working on this project, but in the meantime, check out my top 50 Spotify tracks, which I retrieved using Azure Data Factory (ADF).
+        I'm still working on this project, but in the meantime, check out my top 50 Spotify tracks, which I retrieved using Azure Data Factory (ADF).
       </p>
-      <code className="text-gray-400">
+      <code className="text-gray-400 block overflow-auto break-words">
         https://api.spotify.com/v1/me/top/artists?time_range=medium_term&limit=50&offset=0
       </code>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 mt-5">
