dbbc29ebe3b69ea334a24bd19be9932422a8b743
Nelba Barreto <barretonelba@gmail.com>
2024-08-29 09:34:04 -0400

    Habilitar detección de envío de forms

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -76,60 +76,62 @@ const Contact = () => {
         <div className="border-t border-gray-500 flex-grow ml-3"></div>
       </div>
 
-      {formSubmitted ? <div className="border-purple-700 p-2 w-full bg-purple-100 text-purple-700 rounded-lg">Your message has been sent</div> : <form onSubmit={handleSubmit} className="mt-8 space-y-6" name="contact" netlify>
-        <h3 className="text-lg text-white font-bold">Send me a Message! 💜</h3>
-        <div>
-          <label htmlFor="name" className="block text-white text-sm font-medium">
-            Name
-          </label>
-          <input
-            type="text"
-            id="name"
-            name="name"
-            value={formData.name}
-            onChange={handleChange}
-            className="mt-1 block w-full p-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-            required
-          />
-        </div>
-        <div>
-          <label htmlFor="email" className="block text-white text-sm font-medium">
-            Email
-          </label>
-          <input
-            type="email"
-            id="email"
-            name="email"
-            value={formData.email}
-            onChange={handleChange}
-            className="mt-1 block w-full p-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-            required
-          />
-        </div>
-        <div>
-          <label htmlFor="message" className="block text-white text-sm font-medium">
-            Message
-          </label>
-          <textarea
-            id="message"
-            name="message"
-            value={formData.message}
-            onChange={handleChange}
-            rows="4"
-            className="mt-1 block w-full p-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-            required
-          ></textarea>
-        </div>
-        <div>
-          <button
-            type="submit"
-            className="bg-purple-700 border-purple-950 border px-4 py-2 rounded-lg text-white disabled:bg-purple-400"
-            disabled={!formData.name || !formData.email || !formData.message}
-          >
-            Send
-          </button>
-        </div>
-      </form>}
+      {formSubmitted ? <div className="border-purple-700 p-2 w-full bg-purple-100 text-purple-700 rounded-lg">Your message has been sent</div> :
+        <form onSubmit={handleSubmit} className="mt-8 space-y-6" name="contact" method="post">
+          <h3 className="text-lg text-white font-bold">Send me a Message! 💜</h3>
+          <input type="hidden" name="form-name" value="contact" />
+          <div>
+            <label htmlFor="name" className="block text-white text-sm font-medium">
+              Name
+            </label>
+            <input
+              type="text"
+              id="name"
+              name="name"
+              value={formData.name}
+              onChange={handleChange}
+              className="mt-1 block w-full p-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+              required
+            />
+          </div>
+          <div>
+            <label htmlFor="email" className="block text-white text-sm font-medium">
+              Email
+            </label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
+              className="mt-1 block w-full p-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+              required
+            />
+          </div>
+          <div>
+            <label htmlFor="message" className="block text-white text-sm font-medium">
+              Message
+            </label>
+            <textarea
+              id="message"
+              name="message"
+              value={formData.message}
+              onChange={handleChange}
+              rows="4"
+              className="mt-1 block w-full p-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+              required
+            ></textarea>
+          </div>
+          <div>
+            <button
+              type="submit"
+              className="bg-purple-700 border-purple-950 border px-4 py-2 rounded-lg text-white disabled:bg-purple-400"
+              disabled={!formData.name || !formData.email || !formData.message}
+            >
+              Send
+            </button>
+          </div>
+        </form>}
     </section>
   );
 };
