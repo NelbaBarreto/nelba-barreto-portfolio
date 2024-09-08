c4d2646a93af63a53cf7c910c9cf2154550d5b72
NelbaBarreto <barretonelba@gmail.com>
2024-09-08 00:27:49 -0400

    Utilizar el tab de topArtists por defecto

--- src/temp/Spotify.jsx
+++ src/temp/Spotify.jsx
@@ -1,25 +1,25 @@
 import React, { useState } from "react";
 import classNames from "classnames";
-import { useQueries } from "react-query";
+import { useQuery } from "react-query";
 import { getSpotifyData } from "../api";
 
 const Spotify = () => {
-  const [activeTab, setActiveTab] = useState("tracks");
+  const [activeTab, setActiveTab] = useState("artists");
 
-  const [topTracksQuery, topArtistsQuery] = useQueries([
-    {
-      queryKey: ["topTracks", "raw", "topTracks.json"],
-      queryFn: () => getSpotifyData("raw", "topTracks.json"),
-    },
-    {
-      queryKey: ["topArtists", "raw", "topArtists.json"],
-      queryFn: () => getSpotifyData("raw", "topArtists.json"),
-    },
-  ]);
+  const topArtistsQuery = useQuery(
+    ["topArtists", "raw", "topArtists.json"],
+    () => getSpotifyData("raw", "topArtists.json")
+  );
+
+  const topTracksQuery = useQuery(
+    ["topTracks", "raw", "topTracks.json"],
+    () => getSpotifyData("raw", "topTracks.json"),
+    { enabled: activeTab === "tracks" } // Solo carga los tracks si está activo
+  );
 
   const topTracks = topTracksQuery.data?.items;
   const topArtists = topArtistsQuery.data?.items;
-  const isLoading = topTracksQuery.isLoading || topArtistsQuery.isLoading;
+  const isLoading = activeTab === "tracks" ? topTracksQuery.isLoading : topArtistsQuery.isLoading;
 
   const handleTabClick = (tab) => {
     setActiveTab(tab);
@@ -29,11 +29,11 @@ const Spotify = () => {
 
   return (
     <section className="bg-gray-800 rounded-lg m-5 p-8 border border-gray-700 shadow-lg" id="projects">
-      <p className="text-white">
-        I'm still working on this project, but in the meantime, check out my top Spotify tracks and artists.
-        I connect to the Spotify API daily to retrieve my top 50 tracks and artists. The data is ingested into an Azure Data Lake using an automated pipeline, and I use an Azure Function to fetch the latest data from the lake.
-        The most recent information is then displayed dynamically on this page.
-      </p>
+      <div className="text-white">
+        <p>I'm still working on this project, but in the meantime, check out my top Spotify tracks and artists.</p>
+        <p>I connect to the Spotify API daily to retrieve my top 50 tracks and artists. The data is ingested into an Azure Data Lake using an automated pipeline, and I use an Azure Function to fetch the latest data from the lake.</p>
+        <p>The most recent information is then displayed dynamically on this page.</p>
+      </div>
       <code className="text-gray-400 block overflow-auto break-words">
         {activeTab === "tracks"
           ? "https://api.spotify.com/v1/me/top/tracks?time_range=medium_term&limit=50&offset=0"
@@ -41,16 +41,16 @@ const Spotify = () => {
       </code>
       <div className="flex justify-center space-x-4 mt-5">
         <button
-          className={classNames("border-purple-950 border px-4 py-2 rounded-lg text-white bg-[#0f172a]", { "bg-purple-700": activeTab === "tracks" })}
-          onClick={() => handleTabClick("tracks")}
-        >
-          Top Tracks
-        </button>
-        <button
           className={classNames("border-purple-950 border px-4 py-2 rounded-lg text-white bg-[#0f172a]", { "bg-purple-700": activeTab === "artists" })}
           onClick={() => handleTabClick("artists")}
         >
           Top Artists
+        </button>
+        <button
+          className={classNames("border-purple-950 border px-4 py-2 rounded-lg text-white bg-[#0f172a]", { "bg-purple-700": activeTab === "tracks" })}
+          onClick={() => handleTabClick("tracks")}
+        >
+          Top Tracks
         </button>
       </div>
 
