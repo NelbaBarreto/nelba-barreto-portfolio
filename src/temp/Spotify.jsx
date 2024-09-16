be1f26334feabfca704503f798a41ce58924b61a
Nelba Barreto <barretonelba@gmail.com>
2024-09-16 15:51:02 -0400

    Mejorar loading de pantalla de spotify

--- src/temp/Spotify.jsx
+++ src/temp/Spotify.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import classNames from "classnames";
 import { useQuery } from "react-query";
 import { getSpotifyData } from "../api";
+import Loading from "../components/Loading";
 
 const Spotify = () => {
   const [activeTab, setActiveTab] = useState("artists");
@@ -67,44 +68,48 @@ const Spotify = () => {
   };
 
   return (
-    <section className="bg-gray-800 rounded-lg m-5 p-8 border border-gray-700 shadow-lg" id="projects">
-      <div className="text-white">
-        <p>I'm still working on this project, but in the meantime, check out my top Spotify tracks and artists.</p>
-        <p>I connect to the Spotify API daily to retrieve my top 50 tracks and artists. The data is ingested into an Azure Data Lake using an automated pipeline, and I use an Azure Function to fetch the latest data from the lake.</p>
-        <p>The most recent information is then displayed dynamically on this page.</p>
-      </div>
-      <code className="text-gray-400 block overflow-auto break-words">
-        {activeTab === "tracks"
-          ? "https://api.spotify.com/v1/me/top/tracks?time_range=medium_term&limit=50&offset=0"
-          : "https://api.spotify.com/v1/me/top/artists?time_range=medium_term&limit=50&offset=0"}
-      </code>
-      <div className="flex justify-center space-x-4 mt-5">
-        <button
-          className={classNames("border-purple-950 border px-4 py-2 rounded-lg text-white bg-[#0f172a]", { "bg-purple-700": activeTab === "artists" })}
-          onClick={() => handleTabClick("artists")}
-        >
-          Top Artists
-        </button>
-        <button
-          className={classNames("border-purple-950 border px-4 py-2 rounded-lg text-white bg-[#0f172a]", { "bg-purple-700": activeTab === "tracks" })}
-          onClick={() => handleTabClick("tracks")}
-        >
-          Top Tracks
-        </button>
-      </div>
-
-      {isLoading ? (
-        <span className="text-white">Loading...</span>
-      ) : (
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-6 mt-5">
-          {activeTab === "tracks" ? items?.map((item, idx) => (
-            <TrackCard key={idx} item={item} activeTab={activeTab} />
-          )) : items?.map((item, idx) => (
-            <ArtistCard key={idx} item={item} activeTab={activeTab} />
-          ))}
+    <div id="projects">
+      <section className="bg-gray-800 rounded-lg m-5 p-8 border border-gray-700 shadow-lg">
+        <div className="text-white">
+          <p>I'm actively working on this project, but for now, explore my top Spotify tracks and artists 💚.</p>
+          <p>I connect to the Spotify API daily to retrieve my top 50 tracks and artists, which are then ingested into an Azure Data Lake through an automated Azure Data Factory pipeline.</p>
         </div>
-      )}
-    </section>
+        <code className="text-gray-400 block overflow-auto break-words">
+          {activeTab === "tracks"
+            ? "https://api.spotify.com/v1/me/top/tracks?time_range=medium_term&limit=50&offset=0"
+            : "https://api.spotify.com/v1/me/top/artists?time_range=medium_term&limit=50&offset=0"}
+        </code>
+        <div className="flex justify-center space-x-4 mt-5">
+          <button
+            className={classNames("border-purple-950 border px-4 py-2 rounded-lg text-white bg-[#0f172a]", { "bg-purple-700": activeTab === "artists" })}
+            onClick={() => handleTabClick("artists")}
+          >
+            Top Artists
+          </button>
+          <button
+            className={classNames("border-purple-950 border px-4 py-2 rounded-lg text-white bg-[#0f172a]", { "bg-purple-700": activeTab === "tracks" })}
+            onClick={() => handleTabClick("tracks")}
+          >
+            Top Tracks
+          </button>
+        </div>
+      </section>
+      <section className="bg-gray-800 rounded-lg m-5 p-8 border border-gray-700 shadow-lg">
+        {isLoading ? (
+          <div className="flex justify-center">
+            <Loading />
+          </div>
+        ) : (
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-6 mt-5">
+            {activeTab === "tracks" ? items?.map((item, idx) => (
+              <TrackCard key={idx} item={item} activeTab={activeTab} />
+            )) : items?.map((item, idx) => (
+              <ArtistCard key={idx} item={item} activeTab={activeTab} />
+            ))}
+          </div>
+        )}
+      </section>
+    </div>
   );
 };
 
